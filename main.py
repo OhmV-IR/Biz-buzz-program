6e7e817306bd1c5d7a38423dd14ba7e60ed82709
AB <bourdeaux.adrien@gmail.com>
2023-04-17 14:19:47 -0400

    added comments

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from math import pi
+#ask for user input
 AmountOfNumbersToPrint = input("How many numbers do you want to print: ")
+#initialize array
 arrToPrint = ["Bizz buzz sequence: "]
+# error catching(if try fails run except)
 try:
     AmountOfNumbersToPrint = int(AmountOfNumbersToPrint)
 except:
@@ -8,14 +10,23 @@ except:
     exit()
 # i is a counter variable
 for i in range(AmountOfNumbersToPrint):
+    #if the current number is not divisible by 5 and 3 then check if divisible by 3
+    # then check if it is divisible by 5
     if(i % 3 == 0 and i % 5 == 0):
+        #if divisible by 5 and 3
+        # Add this to the end of the array
         arrToPrint.append("bizz buzz, ")
     elif(i % 3 == 0):
+        #if divisible by 3 then add this to end of arr
         arrToPrint.append("bizz, ")
     elif(i % 5 == 0):
+        #if divisible by 5 then add this to the end of arr
         arrToPrint.append("buzz, ")
     else:
+        #if none is true add the number to the end of arr
         arrToPrint.append((str(i) + ", "))
+# remove all quotation marks and brackets from string
 output = str(arrToPrint).replace("'",'',100000000)
 output = output.replace("[]",'',1000000000)
+#print final string
 print(output)
