c9ea2ecb594d11393d37c4a34043dff8b106672e
AB <bourdeaux.adrien@gmail.com>
2023-04-17 14:16:30 -0400

    finished program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from math import pi
 AmountOfNumbersToPrint = input("How many numbers do you want to print: ")
+arrToPrint = ["Bizz buzz sequence: "]
 try:
     AmountOfNumbersToPrint = int(AmountOfNumbersToPrint)
 except:
@@ -7,4 +8,14 @@ except:
     exit()
 # i is a counter variable
 for i in range(AmountOfNumbersToPrint):
-    print("loop ran")
+    if(i % 3 == 0 and i % 5 == 0):
+        arrToPrint.append("bizz buzz, ")
+    elif(i % 3 == 0):
+        arrToPrint.append("bizz, ")
+    elif(i % 5 == 0):
+        arrToPrint.append("buzz, ")
+    else:
+        arrToPrint.append((str(i) + ", "))
+output = str(arrToPrint).replace("'",'',100000000)
+output = output.replace("[]",'',1000000000)
+print(output)
