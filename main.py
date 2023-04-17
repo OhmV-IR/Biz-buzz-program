bfa5f9b36b8272b826c56fe2ffe640cb4116b974
AB <bourdeaux.adrien@gmail.com>
2023-04-17 13:58:54 -0400

    error catching

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from math import pi
-while(True):
-    # Do bizz buzz seqeunce
-    print("true is true(WOW)")
-    # coding is fun
+AmountOfNumbersToPrint = input("How many numbers do you want to print: ")
+try:
+    AmountOfNumbersToPrint = int(AmountOfNumbersToPrint)
+except:
+    print("Amount of numbers to print is invalid")
+    exit()
+# i is a counter variable
+for i in range(AmountOfNumbersToPrint):
+    print("loop ran")
